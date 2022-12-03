a838943c1cbbaead45937f2f120e6b2da429c298
Muhsin-42 <muhsinofficial21@gmail.com>
2022-12-03 12:10:39 +0530

    connectino pass2

--- connection/conn.js
+++ connection/conn.js
@@ -3,7 +3,7 @@ let dotenv = require('dotenv').config()
 
 
 // mongoose.connect("mongodb://localhost:27017/ecommerce", {
-mongoose.connect(dotenv.parsed.MongoDB, {
+mongoose.connect('mongodb+srv://muhsin:S3z6SNe86y5MmxrD@cluster0.8tkdq4l.mongodb.net/ecommerce', {
     useNewUrlParser:true
 } ).then(()=>{
     console.log( 'Connection successful')
