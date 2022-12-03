8d54fbdf57cd466b05828883c5900193ea13fc54
Muhsin-42 <muhsinofficial21@gmail.com>
2022-12-03 10:42:29 +0530

    connection update

--- connection/conn.js
+++ connection/conn.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/ecommerce", {
+// mongoose.connect("mongodb://localhost:27017/ecommerce", {
+mongoose.connect("mongodb+srv://muhsin:20170119193@cluster0.8tkdq4l.mongodb.net/ecommerce", {
     useNewUrlParser:true
 } ).then(()=>{
     console.log( 'Connection successful')
